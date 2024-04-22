78229e8142b43b679e7f03beebf6590ee1bd8901
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-04-22 17:19:44 +0530

    some-message

--- 06-array.sh
+++ 06-array.sh
@@ -2,6 +2,9 @@
 
 MOVIES=("RRR" "DJTillu" "murari")
 
+# size of above array is 3
+# index are 0,1,2
+
 echo "first movie is: ${MOVIES[0]}"
 echo "first movie is: ${MOVIES[1]}"
 echo "first movie is: ${MOVIES[@]}"
