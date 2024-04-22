16a4fc5f3f7211cc0ea508325c604b524d70b21a
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-04-22 12:30:12 +0530

    some-message

---
+++ 04-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# declearing variable
+Person1=$1
+Person2=$2
+
+#referring variable
+echo $Person1:: "Hi $Person2, how are you?"
+echo $Person2:: "Hello $Person1, i am good. how are you?"
+echo $Person1:: "I am fine too. How is your work?"
+echo $Person2:: "not bad. I am thinking to upgrade to DevOps
