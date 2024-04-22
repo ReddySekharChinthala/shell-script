2f48ce08d5850bea411977bf928354faa8bc8947
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-04-22 11:53:47 +0530

    some-message

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-This is the sample script
+#!/bin/bash
+
+echo Reddy:: "Hi Sekhar, how are you"
+echo Sekhar:: "Hello Reddy, i am good. how are you?"
+echo Reddy:: "I am fine too. How is your work?"
+echo Sekhar:: "not bad. I am thinking to upgrade to DevOps"
