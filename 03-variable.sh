ed624381996b70e622d5205fd6b00c52e5637ca6
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-04-22 12:13:15 +0530

    some-message

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Suresh # no spaces between=and values
+Person2=Dinesh
+
+echo $Person1:: "Hi Sekhar, how are you"
+echo $Person2:: "Hello Reddy, i am good. how are you?"
+echo $Person1:: "I am fine too. How is your work?"
+echo $Person2:: "not bad. I am thinking to upgrade to DevOps"
