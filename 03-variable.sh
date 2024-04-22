b8d114e3458d6c9168b5d99970e21dd105d47648
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-04-22 12:19:39 +0530

    some-message

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# declearing variable
 Person1=Suresh # no spaces between=and values
 Person2=Dinesh
 
+#referring variable
 echo $Person1:: "Hi $Person2, how are you?"
 echo $Person2:: "Hello $Person1, i am good. how are you?"
 echo $Person1:: "I am fine too. How is your work?"
