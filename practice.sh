80e23e623675ca78e5b43d4d3522d48e39e59984
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-04-23 11:28:59 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Reddy: Hello Sekhar, how are you?"
-echo "Sekhar: Hi Reddy, im fine. how are you?"
-echo "Reddy: How is work going on?"
+Person1=suresh
+Person2=dinesh
+
+echo "$Person1: Hello $Person2, how are you?"
+echo "Sekhar: Hi $Person1, im fine. how are you?"
+echo "$Person1: How is work going on?"
 echo "Not bad. im thinking to change my domain to devops"
