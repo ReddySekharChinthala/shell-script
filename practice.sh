68daa08b881cd559d5b633d54b2eaecfd466bcea
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-04-23 11:53:16 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -6,6 +6,6 @@ echo "first movie is: ${movies[0]}"
 
 echo "first movie is: ${movies[1]}"
 
-echo "first movie is: ${movie[2]}"
+echo "first movie is: ${movies[2]}"
 
-echo "first movie is: ${movie[$@]}"
+echo "first movie is: ${movies[$@]}"
