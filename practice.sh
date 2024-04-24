8d4d80c5e20c9e7b9aaaf4de1ba37d7d81b1d08f
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-04-24 15:33:36 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -2,20 +2,20 @@
 
 USERNAME=$(id -u)
 
-if [ $USERNAME -ne 0 ]
-then 
+if [ $USERNAME -ne 0]
+then
     echo "please use the root access"
     exit 1
-else
-    echo "your a super user"
+else 
+    echo your a super user
 fi
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]
+if [ $? -ne 0]
 then
-    echo "installation mysql..failure"
+    echo "Installation mysql...Failure"
     exit 1
 else
-    echo "installation mysql..success"
+    echo "Installation mysql success"
 fi
