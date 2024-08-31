91546e61378cd028ffa097702125c14eb6427fe4
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-08-31 20:14:06 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-USERNAME=$(id -u)
 
-VALIDATE(){
-    echo "Exit status: $1"
-    echo "What are you doing: $2"
-}
-
-if [ $USERNAME -ne 0]
-then
-    echo "please use the root access"
-    exit 1
-else 
-    echo your a super user
-fi
-
-dnf install mysql -y
-VALIDATE $? "Installation...Mysql"
-
-
-dnf install mysql -y
-VALIDATE $? "Installation...git"
-
+echo Reddy:: "Hi, Sekhar HOw are you?"
+echo Sekhar:: "Iam good Reddy, what about you"
