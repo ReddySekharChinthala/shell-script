f25a2907a8e05ee32781e83863fb12658d7780d5
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-08-31 20:50:27 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -5,4 +5,5 @@ MOVIES=("RRR" "DJTillul" "Murari")
 echo "first movie name is: ${MOVIES[0]}"
 echo "second movie name is: ${MOVIES[2]}"
 echo "third movie name is: ${MOVIES[1]}"
+echo "All variables : ${MOVIES[@]}"
 echo "All variables: $@"
