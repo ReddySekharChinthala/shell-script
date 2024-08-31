3b462ca19fc998dc0fd9798d3f18b3e208c1351c
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-08-31 20:32:20 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -5,3 +5,11 @@ Person2=$2
 
 echo $Person1:: "Hi, $Person2 HOw are you?"
 echo $Person2:: "Iam good $Person1, what about you?"
+
+echo "print your username:"
+
+read "USERNAME"
+
+echo "print the password:"
+
+read "PASSWORD"
