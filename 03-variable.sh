84b5b49674407a06ecad0eb7cd57c29d5ab497ea
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-23 20:54:12 +0530

    reffering variables

--- 03-variable.sh
+++ 03-variable.sh
@@ -5,7 +5,7 @@ Person1=Reddy #no spaces between person and =
 Person2=Pavithra
 
 #referring variable
-echo "Person1: Hello Person2, how are you?"
-echo "Person2: Hi Person1, iam good. How are you?"
-echo "Person1: i'm fine. where your working?"
-echo "Person2: I'm working in ECW."
+echo "$Person1: Hello $Person2, how are you?"
+echo "$Person2: Hi $Person1, iam good. How are you?"
+echo "$Person1: i'm fine. where your working?"
+echo "$Person2: I'm working in ECW."
