82c7364d5433a17f176f411fe223b6fee256378c
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-23 22:15:58 +0530

    reffering variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "please enter username::"
+echo "Please enter username::"
 
-read -s USERNAME  # here USERNAME is variable
+read -s USERNAME
 
-echo "please enter password::"
+echo "Please enter password::"
 
 read -s PASSWORD
 
-echo "username is: $USERNAME, password is: $PASSWORD"
+echo "username is: $USERNAME, password is $PASSWORD"
