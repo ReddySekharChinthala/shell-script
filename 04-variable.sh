a36bba1a9cef4212920be8dcc521b13989d7c1a9
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-23 21:11:20 +0530

    reffering variables

--- 04-variable.sh
+++ 04-variable.sh
@@ -5,7 +5,7 @@ Person1=$1
 Person2=$2
 
 #referring variable
-echo $Person1:: "Hi $Person2, how are you?"
-echo $Person2:: "Hello $Person1, i am good. how are you?"
-echo $Person1:: "I am fine too. How is your work?"
-echo $Person2:: "not bad. I am thinking to upgrade to DevOps"
+echo "$Person1: Hello $Person2, how are you?"
+echo "$Person2: Hi $Person1, iam good. How are you?"
+echo "$Person1: i'm fine. where your working?"
+echo "$Person2: I'm working in ECW."
