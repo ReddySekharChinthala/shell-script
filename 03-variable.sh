039d5e21ba610a379a9e8a42d4a898810ea7eb6a
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-29 15:11:04 +0530

    Shell Script

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # declearing variable
-$Person1=Reddy #no spaces between person and =
-$Person2=Pavithra
+Person1=Reddy #no spaces between person and =
+Person2=Pavithra
 
 #referring variable
-echo "$Person1:: Hi, $Person2. How are you?"
+echo "$Person1:: Hi $Person2. How are you?"
 echo "$Person2:: Hi $Person1, i'm good. What about you?"
 echo "$Person1:: im fine. Where you are working?"
 echo "$Person2:: Iam working in ECW as a TeamManager."
