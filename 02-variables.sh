8fc121c4712a6380e842312c9445140f926580cb
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-29 15:02:50 +0530

    Shell Script

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,5 +3,5 @@
 echo "Reddy:: Hi, Pavithra. How are you?"
 echo "Pavithra:: Hi Reddy, i'm good. What about you?"
 echo "Reddy:: im fine. Where you are working?"
-echo "Pavithra:: Iam working in ECL as a TeamManager."
+echo "Pavithra:: Iam working in ECW as a TeamManager."
 echo "Reddy:: Good, Iam also working in IT and planning to switch as a DevOPs engineer." 
