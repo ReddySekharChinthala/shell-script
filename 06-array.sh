09136221cc2f9ff4d3c090f9297c8ae9fa2efd6e
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-29 15:27:11 +0530

    Shell Script

--- 06-array.sh
+++ 06-array.sh
@@ -6,3 +6,6 @@ Tools=("Shellscript" "Ansible" "Terraform" "Git" "Jenkins" "Docker" "Kubernetes"
 #and index of arry is 0 to 6
 
 echo "Print the first Tool in DevOps:: ${Tools[0]}"
+echo "Print the second Tool in DevOps:: ${Tools[1]}"
+echo "Print the third Tool in DevOps:: ${Tools[2]}"
+echo "Print the all Tools in DevOps:: ${Tools[@]}"
