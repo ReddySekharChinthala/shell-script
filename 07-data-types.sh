1fe60875ce3464a2bed00f566ab6ece5378cf024
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-29 15:32:29 +0530

    Shell Script

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -4,5 +4,14 @@ No1=$1
 No2=$2
 
 SUM=$(($No1+$No2))
+substraction=$(($No1-$No2))
+devide=$(($No1/$No2))
+percentile=$(($No1%$No2))
 
 echo "Total no.of $No1 and $No2 is: $SUM"
+
+echo "Total no.of $No1 and $No2 is: $substraction"
+
+echo "Total no.of $No1 and $No2 is: $devide"
+
+echo "Total no.of $No1 and $No2 is: $percentile"
