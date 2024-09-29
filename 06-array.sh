31ae6d36410b1fa8a2f7ce48e8345d54046ea63c
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-29 15:25:06 +0530

    Shell Script

--- 06-array.sh
+++ 06-array.sh
@@ -5,4 +5,4 @@ Tools=("Shellscript" "Ansible" "Terraform" "Git" "Jenkins" "Docker" "Kubernetes"
 #here the the size of arry is 7.
 #and index of arry is 0 to 6
 
-echo "Print the first Tool in DevOps:: ($Tool[0])
+echo "Print the first Tool in DevOps:: ${Tool[0]}"
