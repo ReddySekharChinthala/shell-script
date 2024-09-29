03b99cbb4e2f21a58b49054cdc56504c29e6fb84
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-29 22:15:23 +0530

    Shell Script

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,33 +1,27 @@
 #/bin/bash
-
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-then
+then 
     echo "Please run the script with root access."
-    exit 1
 else
-    echo "your a super user"
+    echo "you are super user."
 fi
 
 dnf install mysql -y
 
-if ( $? -eq 0 )
+if [ $? -ne 0 ]
 then
-    echo "Installation of mysql is.... failure."
-    exit 1
-else 
-    echo "Installation of mysql is.... SUCCESS."
+    echo "Installation of Mysql...FAILURE."
+else
+    echo "Installation of Mysql...SUCCESS."
 fi
 
 dnf install git -y
 
-if ( $? -eq 0 )
+if [ $? -ne 0 ]
 then
-    echo "Installation of git is.... failure."
-    exit 1
+    echo "Installation of git...FAILURE."
 else
-    echo "Installation of git is....SUCCESS"
+    echo "Installation of git...SUCCESS."
 fi
-
-echo "Script is proceeding?"
