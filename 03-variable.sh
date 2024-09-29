ac9016cc6493f8a7ef5ec5db8a70a29f8985d445
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-29 15:05:10 +0530

    Shell Script

--- 03-variable.sh
+++ 03-variable.sh
@@ -5,7 +5,8 @@ Person1=Reddy #no spaces between person and =
 Person2=Pavithra
 
 #referring variable
-echo "$Person1: Hello $Person2, how are you?"
-echo "$Person2: Hi $Person1, iam good. How are you?"
-echo "$Person1: i'm fine. where your working?"
-echo "$Person2: I'm working in ECW."
+echo "$Person1:: Hi, $Person2. How are you?"
+echo "$Person2:: Hi $Person1, i'm good. What about you?"
+echo "Reddy:: im fine. Where you are working?"
+echo "Pavithra:: Iam working in ECW as a TeamManager."
+echo "Reddy:: Good, Iam also working in IT and planning to switch as a DevOPs engineer." 
