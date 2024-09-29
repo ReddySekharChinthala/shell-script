6eff55dbb1a3cdd79ea4e5a9f198159b75c7d87d
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-29 22:23:49 +0530

    Shell Script

--- 11-fumctions.sh
+++ 11-fumctions.sh
@@ -1,31 +1,23 @@
-
-#!/bin/bash
-
+#/bin/bash
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo "$2...FAILURE"
-        exit 1
-    else
-        echo "$2...SUCCESS"
-    fi
+Validation(){
+    echo "Exit status is: $1"
+    echo "What are you doing: $2"
 }
 
 if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+then 
+    echo "Please run the script with root access."
+    exit 1
 else
-    echo "You are super user."
+    echo "you are super user."
 fi
 
-dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MySQL"
+dnf install mysql -y
+Validation $? "Installating mysql"
 
-dnf install git -y &>>$LOGFILE
-VALIDATE $? "Installing Git"
+dnf install git -y
+Validation $? "Installating git"
+
+
