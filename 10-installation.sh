126fa1693788fb85c14693d6dd85f51c08205506
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-29 16:16:32 +0530

    Shell Script

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,3 +1,16 @@
 #/bin/bash
 
+USERID=$(id -u)
+
+if ( $USERID -ne 0 )
+then
+    echo "Please run the script with root access"
+else
+    echo "your a super user"
+fi
+
 dnf installing mysql -y
+
+# if ( $? -eq 0 )
+# then
+#     echo ""
