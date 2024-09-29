d41822145e7ed27d6152c42c04c680276abfa6b3
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-29 16:18:02 +0530

    Shell Script

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,9 +2,9 @@
 
 USERID=$(id -u)
 
-if ( $USERID -ne 0 )
+if [ $USERID -ne 0 ]
 then
-    echo "Please run the script with root access"
+    echo "Please run the script with root access."
 else
     echo "your a super user"
 fi
