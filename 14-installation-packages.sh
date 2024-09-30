c1aabd4cbd385d470338dd701eec405b80991b1f
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-09-30 22:11:49 +0530

    install_packages

--- 14-installation-packages.sh
+++ 14-installation-packages.sh
@@ -12,10 +12,10 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2--is FAILURE."
+        echo "$2--is...$R FAILURE. $N"
         exit 1
     else
-        echo "$2..is SUCCESS."
+        echo "$2..is...$G SUCCESS. $N"
     fi
 }
 
