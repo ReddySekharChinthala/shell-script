4ec59bce4efc6b46f6d39c7d3bd6ca765d0f7015
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-24 18:59:13 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -4,4 +4,4 @@ Person1=$1
 Person2=$2
 
 echo "$Person1:: Hi $Person2, how are you..?"
-echo "$Person2:: Hello $Person1, iam good, how are you..?
+echo "$Person2:: Hello $Person1, iam good, how are you..?"
