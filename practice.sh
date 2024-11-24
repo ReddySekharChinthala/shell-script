da8f1ed9d6973c67c224eae7c506a2e264f7d76a
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-24 19:26:06 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -2,6 +2,6 @@
 
 movies=("Pushpa1" "Pushpa2" "Reddy")
 
-echo "First movie is:: $movies[0]"
-echo "Second movie is:: $movies[1]"
-echo "Third movie is:: $movies[2]"
+echo "First movie is:: $ movies[0]"
+echo "Second movie is:: $ movies[1]"
+echo "Third movie is:: $ movies[2]"
