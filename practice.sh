ed066c0835afb51e4f8a557b71ee73a6a6319844
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-24 20:23:25 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please run the script with root access."
-    exit 1
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo "you are a super user."
+    echo "You are super user."
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]
 then
-    echo "Installation of mysql is..Failure."
+    echo "Installation of mysql...FAILURE"
     exit 1
 else
-    echo "Installation of mysql is..Success."
+    echo "Installation of mysql...SUCCESS"
 fi
  
