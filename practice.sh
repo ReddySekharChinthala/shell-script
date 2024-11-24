9ef2c73ab7fbac58c5cf072fcbda311584f545c5
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-24 18:50:55 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
-MOVIES=("RRR" "DJTillul" "Murari")
+Person1=$1
+Person2=$2
 
-echo "first movie name is: ${MOVIES[0]}"
-echo "second movie name is: ${MOVIES[2]}"
-echo "third movie name is: ${MOVIES[1]}"
-echo "All variables : ${MOVIES[@]}"
-echo "All variables: $@"
-echo "Number of variable passed: $#"
-echo "Script name: $0"
-echo "current working directory: $PWD"
-echo "Home directory of current user: $HOME"
-echo "which use is working this script: $USER"
-echo "Process ID: $$"
-
-
-NUMBER=$5
-
-if [$NUMBER is -gt 10]
-then
-    echo "Given number $NUMBER is greater then 10"
-
-else
-
-    echo "Given number $NUMBER is less then 10"
-
-fi
+echo "$Person1:: Hi $Person2, how are you..?"
+echo "$Person2:: Hello $Person1, iam good, how are you..?
