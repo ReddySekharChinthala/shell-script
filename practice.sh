184fe703f9c3f687d3cdd3196f6b21caaca1ff9b
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-24 21:13:20 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -2,7 +2,15 @@
 
 USERID=$(id -u)
 
-validate() {
+validate(){
+if [ $1 -ne 0 ]
+then
+    echo "$2 is.. Failure."
+else
+    echo "$2 is..Success."
+fi
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access."
@@ -10,7 +18,7 @@ then
 else
     echo "You are super user."
 fi
-}
+
 
 dnf install mysql -y
 Validate $? "Installing mysql"
