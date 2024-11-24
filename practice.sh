ab172ad235b03a840da9a5c4be25246843583075
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-24 20:16:36 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+USEID=$(id -u)
+
+if [ $USEID -ne 0 ]
+then
+    echo "Please run the script with root access."
+    exit 1
+else
+    echo "you are a super user."
+fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]
 then
-    echo "INstallation of mysql is..Failure."
+    echo "Installation of mysql is..Failure."
+    exit 1
 else
-    echo "INstallation of mysql is..Success."
+    echo "Installation of mysql is..Success."
 fi
  
