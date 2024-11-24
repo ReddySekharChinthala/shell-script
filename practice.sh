65518babc8a864036f41ab0e28b5214e15ec70d4
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-24 22:10:57 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%s)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R=\e[31m
+G=\e[32m
+N=\e[0m
 
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-    echo "$2 is.. Failure."
+    echo "$2 is..$R Failure $N"
+    exit 1
 else
-    echo "$2 is..Success."
+    echo "$2 is..$G Success $N"
 fi
 }
 
@@ -19,10 +26,10 @@ else
     echo "You are super user."
 fi
 
-dnf install mysql -y
+dnf install mysql -y &>>LOGFILE
 
 VALIDATE $? "Installing mysql"
 
-dnf install git -y
+dnf install git -y &>>LOGFILE
 
 VALIDATE $? "Installing git"
