f69fafc49c40168bd12fb5f30ea72030eb630607
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-26 20:57:42 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -1,35 +1,3 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%s)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 is..$R Failure. $N"
-        exit 1
-    else
-         echo -e "$2 is..$G Success. $N"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
-fi
-
-dnf install mysql -y &>>LOGFILE
-
-VALIDATE $? "Installing mysql"
-
-dnf install git -y &>>LOGFILE
-
-VALIDATE $? "Installing git"
+echo "Hello, Myself ReddySekhar. iam a DevOps Engoneer."
