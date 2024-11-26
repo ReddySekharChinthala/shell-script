cb09a552277f0304eb06340438f4cc752b3c91c6
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-26 21:00:04 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+#Hello World print
 echo "Hello, Myself ReddySekhar. iam a DevOps Engoneer."
+
+echo "Reddy:: Hi Pavithra, how are you..?"
+echo "Pavithra:: Hi Reddy, iam good. How are you..?"
