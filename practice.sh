c47e31bb80b99205f1eb4704be7a7cc9ae41c496
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-26 21:06:25 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -3,8 +3,10 @@
 #Hello World print
 echo "Hello, Myself ReddySekhar. iam a DevOps Engoneer."
 
-Person1=Reddy
-Person2=Pavithra
+#Declaring variable
+Person1=$1
+Person2=$2
 
+#Reffering variable
 echo "$Person1:: Hi $Person2, how are you..?"
 echo "$Person2:: Hi $Person1, iam good. How are you..?"
