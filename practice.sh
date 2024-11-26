bb5e62272041a593eda8e4c3109afaee0209ff49
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-26 21:53:25 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -10,3 +10,8 @@ Person2=$2
 #Reffering variable
 echo "$Person1:: Hi $Person2, how are you..?"
 echo "$Person2:: Hi $Person1, iam good. How are you..?"
+
+# For securing our details
+
+echo "please enter your username"
+read -s "USERNAME is::"
