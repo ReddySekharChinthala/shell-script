21315dfe8e4bde6a6fb9e6877040a3506e0f3c6a
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-26 23:11:45 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -41,5 +41,5 @@
 echo "all variables:$@"
 echo "Number of variables passed:$#"
 echo "script name:$0"
-# echo "current working directory::$PWD"
+echo "current working directory::$PWD"
 # echo "home directory of current user:$HOME"
