bf7e5f4cfedaaae9bcd4ddefb7cdfa5f260fe938
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-11-26 22:04:45 +0530

    Practice

--- practice.sh
+++ practice.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-#Hello World print
-echo "Hello, Myself ReddySekhar. iam a DevOps Engoneer."
+# #Hello World print
+# echo "Hello, Myself ReddySekhar. iam a DevOps Engoneer."
 
-#Declaring variable
-Person1=$1
-Person2=$2
+# #Declaring variable
+# Person1=$1
+# Person2=$2
 
-#Reffering variable
-echo "$Person1:: Hi $Person2, how are you..?"
-echo "$Person2:: Hi $Person1, iam good. How are you..?"
+# #Reffering variable
+# echo "$Person1:: Hi $Person2, how are you..?"
+# echo "$Person2:: Hi $Person1, iam good. How are you..?"
 
-# For securing our details
+# # For securing our details
 
-echo "please enter your username::"
-read "USERNAME"
+# echo "please enter your username::"
+# read "USERNAME"
 
-echo "Please enter your password::"
-read -s "PASSWORD"
+# echo "Please enter your password::"
+# read -s "PASSWORD"
 
-echo "username is:: $USERNAME and password is:: $PASSWORD"
+#echo "username is:: $USERNAME and password is:: $PASSWORD"
+
+## Array's##
+Names=("Reddy" "Sekhar" "Pavithra")
+
+echo "First name is:: ${Names[0]}"
+echo "Second name is:: ${Names[1]}"
+echo "All name list is:: ${Names[@]}"
