90d6362e59260089eceeb3229ffca76aa3a6a17c
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-12-28 21:40:06 +0530

    some-message

--- practice.sh
+++ practice.sh
@@ -4,9 +4,9 @@ number=$1
 
 if [ $number -gt 20 ]
 then
-    echo "given number is greaterthan 5"
+    echo "given number is greaterthan 10"
 else
-    echo "given number is lessthan 5"
+    echo "given number is lessthan 10"
 fi
 
 
