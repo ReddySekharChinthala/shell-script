64f78685139f0eec1c1df105505a90e6bbf0423f
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-12-28 20:09:04 +0530

    some-message

--- practice.sh
+++ practice.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-## Declaring Variable ##
-Person1=$1
-Person2=$2
 
-## reffering variable ##
-echo "$Person1:: Hi, $Person2, How are you.?"
-echo "$Person2:: Hi $Person1, iam good. How about you..?"
-echo "$Person1:: Iam fine and What do you do for living..?"
-echo "$Person2:: Iam working as software Engineer."
+echo "print username is::"
+
+read -s "USERNAME"
+
+echo "print password is::"
+
+read -s "PASSWORD"
+
+echo "username is::$USERNAME and password is::$PASSWORD"
+
