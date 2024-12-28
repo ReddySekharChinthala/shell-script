a8a21c8aceae2b3b4f3a28ee9aeb5067896d0ce0
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-12-28 20:26:29 +0530

    some-message

--- practice.sh
+++ practice.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-Tools=("Shell Script" "Ansible" "Terraform" "Jenkins" "GitHub" "Docker" "Kuberneties")
+no1=$1
+no2=$2
 
-echo "1st tool is:: ${Tools[0]}"
+SUM=$(($no1+$no2))
+Substraction=$(($no1-$no2))
 
-echo "2nd tool is:: ${Tools[1]}"
+echo "Total no. of $no1 and $no2 is::$SUM"
 
-echo "3rd tool is:: ${Tools[2]}"
-
-echo "4th tool is:: ${Tools[3]}"
-
-echo "5th tool is:: ${Tools[4]}"
-
-echo "6th tool is:: ${Tools[5]}"
-
-echo "7th tool is:: ${Tools[6]}"
-
+echo "Total no. of $no1 and $no2 is::$SUbstraction"
 
 
