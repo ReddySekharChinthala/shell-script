ea6f19d5ee0903973300c3d415a801c7a4360048
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2024-12-28 19:57:20 +0530

    some-message

--- practice.sh
+++ practice.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Hi, i'm learning devops course"
+echo "Reddy:: Hi, Pavithra, How are you.?"
+echo "Pavithra:: Hi Reddy, iam good. How about you..?"
+echo "Reddy:: Iam fine and What do you do for living..?"
+echo "Pavithra:: Iam working as software Engineer."
