6debf71d74ec47a3434b340dc0dda5bd77cb0b3b
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-01-15 15:50:24 +0530

    script-call

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -14,3 +14,11 @@ else
     echo -e "$R Please make sure $Source_Directory is exists $N"
     exit 1
 fi
+
+FILES=$(find $Source_Directory -name "*.log" -mtime +14)
+
+while IFS= read -r line
+do
+    echo "Deleting file: $line"
+    rm -rf $line
+done <<< $FILES
