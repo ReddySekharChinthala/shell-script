20159ca631486b22c9d503b01a280787a9d26c4a
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-01-15 15:42:26 +0530

    script-call

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -5,11 +5,12 @@ Source_Directory=/tmp/app-logs
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0m
+N="\e[0m"
 
 if [ -d $Log_Directory ]
 then
     echo -e "$G Source Directory is exists $N"
 else
     echo -e "$R Please make sure $Source_Directory is exists $N"
+    exit 1
 fi
