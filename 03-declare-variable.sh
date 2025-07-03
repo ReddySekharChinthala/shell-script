2025e37c7b66ec929b7f99d1d612c84c24a2644e
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-03 20:58:44 +0530

    variable

--- 03-declare-variable.sh
+++ 03-declare-variable.sh
@@ -5,6 +5,7 @@
 Person1=Reddy
 Person2=Sekhar
 
+#reffering variable
 echo "$Person1:: HI $Person2, How are you..?"
 echo "$Person2:: Hi $Person1, im good. How are you..?"
 echo "$Person1:: Yeah, im good too. Which course your currently learning."
