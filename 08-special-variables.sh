8f9fe68ba328e6e8d02ca833ded9ffff95afab3e
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-03 22:45:43 +0530

    variable

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,3 +3,8 @@
 echo "All variables: $@"
 echo "Number of passed variables: $#"
 echo "script name is : $0"
+echo "current working directory: $PWD"
+echo "home directory of current user: $HOME"
+echo "which user is running the script: $USER"
+echo "Hostname is: $HOSTNAME"
+echo "process ID of current shell script: $$"
