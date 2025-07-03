da760a865e191562527cc51377b9d4989f6a2962
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-03 20:58:16 +0530

    variable

---
+++ 03-declare-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring variable
+
+Person1=Reddy
+Person2=Sekhar
+
+echo "$Person1:: HI $Person2, How are you..?"
+echo "$Person2:: Hi $Person1, im good. How are you..?"
+echo "$Person1:: Yeah, im good too. Which course your currently learning."
+echo "$Person2:: Actually, iam learning AWS with DevOps course."
