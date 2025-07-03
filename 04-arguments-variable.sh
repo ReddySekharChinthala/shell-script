2d35e819f460386f66aecf5d322a459910bbb170
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-03 21:02:40 +0530

    variable

---
+++ 04-arguments-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person=$1
+Person=$2
+
+echo "$Person1:: HI $Person2, How are you..?"
+echo "$Person2:: Hi $Person1, im good. How are you..?"
+echo "$Person1:: Yeah, im good too. Which course your currently learning."
+echo "$Person2:: Actually, iam learning AWS with DevOps course."
