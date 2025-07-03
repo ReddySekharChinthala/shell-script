6720c27681583cedc6711d7b49e6f962137c9441
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-03 20:54:02 +0530

    variable

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Reddy:: HI Sekhar, How are you..?"
+echo "Sekhar:: Hi Reddy, im good. How are you..?"
+echo "Reddy:: Yeah, im good too. Which course your currently learning."
+echo "Sekhar:: Actually, iam learning AWS with DevOps course."
