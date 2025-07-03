f0b4ececb4f5a731fdba05578f44a125efc71fea
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-03 22:13:08 +0530

    variable

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("RRR" "war2" "dhoom2")
+
+echo "first movie name is ${MOVIES[0]}"
+echo "second movie name is ${MOVIES[1]}"
+echo "third movie name is ${MOVIES[2]}"
+echo "all movie names ${MOVIES[@]}
