54c19cd74466f4b6ebd2125fcc1fff814d8f5229
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-05 20:45:41 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -11,13 +11,14 @@ else
 fi
 
 VALIDATE(){
-    
+
     if [ $1 -ne 0 ]
     then
         echo "$2...is FAILURE"
         exit 1
     else
         echo "$2...is SUCCESS"
+    fi
 }
 
 dnf install mysql -y 
