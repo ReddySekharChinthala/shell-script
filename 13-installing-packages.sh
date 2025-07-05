c2fe32c9703cb0528a033f934411d952bf0cdfb4
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-05 22:37:30 +0530

    functions

---
+++ 13-installing-packages.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is.... $R FAILING $N"
+        exit 1
+    else
+        echo -e "$2 is.... $G SUCCESS $N"
+    fi
+}
+
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run the script with super user access."
+    exit 1
+else
+    echo "You are a super user."
+fi
+
+for i is $@
+do 
+    echo "package to install is $i"
+    dnf list installed package is $i &>>LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i is already installed...$Y SKIPPING $N"
+    else
+        dnf install -i -y &>>LOGFILE
+        VALIDATE $? "Installation of $i"
+    fi
+done
