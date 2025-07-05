768c48f21639f77993cf951eaad7dc9721e658ce
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-05 20:33:34 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -19,3 +19,13 @@ then
 else
     echo "Installation of MYSQL is....SUCCESS."
 fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Instllation of git is.....FAILURE"
+    exit 1
+else
+    echo "Installation of git is....SUCCESS"
+fi
