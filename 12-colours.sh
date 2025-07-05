8e3b0e0514740a54267cbc1342671235b19a06c0
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-05 22:08:46 +0530

    functions

--- 12-colours.sh
+++ 12-colours.sh
@@ -5,6 +5,10 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 echo "Script started at $TIMESTAMP"
 
@@ -30,7 +34,10 @@ VALIDATE(){
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MYSQL"
 
-
 dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing git"
+
+dnf install docker -y &>>$LOGFILE
+VALIDATE $? "Installing docker"
+
 
