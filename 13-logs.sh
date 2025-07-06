7e6de852e2e0026e6dbd3ff731fed0656c4ee038
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-06 09:33:22 +0530

    hello-world

---
+++ 13-logs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+echo "Script started at: $TIMESTAMP
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run the script with root access."
+    exit 1
+else
+    echo "You are a super user."
+fi
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is.....FAILING"
+        exit 1
+    else 
+        echo "$2 is.....SUCCESS"
+    fi
+    
+}
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MYSQL"
+
+dnf install docker -y &>>$LOGFILE
+VALIDATE $? "Installing docker"
