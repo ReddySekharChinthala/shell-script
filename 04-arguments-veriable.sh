c3f0b145bdd60c21e6a9af745ed3ce98274521df
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-06 08:06:23 +0530

    hello-world

---
+++ 04-arguments-veriable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: Hi, $person2. How are you?"
+echo "$person2:: Hello $person1, I'm good. what about you?"
+echo "$person1:: yeah iam good. Where your working in BLR?"
+echo "$person2:: Iam working ECW company"
