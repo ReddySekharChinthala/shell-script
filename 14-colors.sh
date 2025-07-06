e48c5b4fd89ab632f5471cbc3b0ed8593923bf3e
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-06 09:48:39 +0530

    hello-world

--- 14-colors.sh
+++ 14-colors.sh
@@ -28,7 +28,7 @@ VALIDATE(){
         echo -e "$2 is.....$R FAILING $N"
         exit 1
     else 
-        echo "$2 is.....$G SUCCESS $N"
+        echo -e "$2 is.....$G SUCCESS $N"
     fi
     
 }
