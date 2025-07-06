fd144ecda1f008b62017ec793761ce0ef3750e04
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-06 08:18:14 +0530

    hello-world

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MOVIES=("RRR" "dhoom2" "war2")
+
+echo "first movie name is:: ${MOVIES[0]}"
+echo "second movie name is:: ${MOVIES[1]}"
+echo "first movie name is:: ${MOVIES[2]}"
+echo "all movies are:: ${$@}"
