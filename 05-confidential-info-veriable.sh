97a96f54dd454f2c889d3c01ac008b64315bef38
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-06 08:12:57 +0530

    hello-world

---
+++ 05-confidential-info-veriable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter the username::"
+
+read -s USERNAME
+
+echo "please enter the password::"
+
+read -s PASSWORD
+
+echo "username is:: $USERNAME and password is:: $PASSWORD
