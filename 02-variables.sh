8f7d0790710fcf3d084aebbdfd686a9f994fd31d
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-06 07:57:06 +0530

    hello-world

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Reddy:: Hi, Pavithra. How are you?"
+echo "Pavithra:: Hello Reddy, I'm good. what about you?"
+echo "Reddy:: "yeah iam good. Where your working in BLR?"
+echo "Pavithra:: Iam working in EWC company."
+
+
