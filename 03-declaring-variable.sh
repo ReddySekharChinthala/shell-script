4eb1fc1152ab2caf9d02efee7e507336b5ae89a1
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-06 08:04:42 +0530

    hello-world

---
+++ 03-declaring-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring the veriable
+person1=Reddy
+person2=Pavithra
+
+#reffering variable
+echo "$person1:: Hi, $person2. How are you?"
+echo "$person2:: Hello $person1, I'm good. what about you?"
+echo "$person1:: yeah iam good. Where your working in BLR?"
+echo "$person2:: Iam working ECW company"
