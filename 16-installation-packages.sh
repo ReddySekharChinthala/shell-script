6aa8318317ccf4db161de4e43ddb50234618d5ab
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-07-15 23:21:00 +0530

    loops

---
+++ 16-installation-packages.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="e\[31m"
+G="e\[32m"
+Y="e\[33m"
+N="e\[34m"
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run script with root access."
+    exit 1
+else
+    echo "Your are super user."
+fi
+
+Validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failure $N"
+        exit 1
+    else
+        echo "$2 is $G SUCCESS $N"
+    fi
+}
+
+for i in $@
+do 
+    echo "package to install: $i"
+    dnf list installed $i
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i package is already installed $Y SKIPPING $N"
+        exit 1
+    else
+        dnf install $i &>>LOGFILE
+        Validate $? "Installation of $i"
+    fi
+done
+
+
+
+
+
