40eeba84c32fb3e0990bf8298b503853f3685e32
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-08-15 22:38:12 +0530

    current-script

--- 17-current-script.sh
+++ 17-current-script.sh
@@ -5,6 +5,6 @@ COURSE="Devops from current script"
 echo "Before calling other script course: $COURSE"
 echo "PID of current script: $$"
 
-./18-other-script.sh
+source ./18-other-script.sh
 
 echo "After calling other script course: $COURSE"
