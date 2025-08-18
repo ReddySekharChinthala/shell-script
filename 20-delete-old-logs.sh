5ff8f5c927a6306c0331f3e865a10a82af801a9f
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2025-08-18 22:47:45 +0530

    new-file

--- 20-delete-old-logs.sh
+++ 20-delete-old-logs.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-SOURCE_DiRECTORY=/tmp/app-logs
+SOURCE_DIRECTORY=/tmp/app-logs
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ -d $SOURCE_DiRECTORY ]
+if [ -d $SOURCE_DIRECTORY ]
 then
     echo -e "$G Source directory is exist $N"
 else
-    echo -e "$R Please make sure $SOURCE_DiRECTORY to exist $N"
+    echo -e "$R Please make sure $SOURCE_DIRECTORY to exist $N"
     exit 1
 fi
 
-FILES=$(find $SOURCE_DiRECTORY -name "*.log" -mtime +14)
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
-echo "deleting the file: $FILES"
+while IFS= read -r line #internal filed separator to check each line in FILES and give to while
+do 
+    echo "Deleting file: $line" #$line vise will delete the files
+
+done <<< $FILES
 
 
 
