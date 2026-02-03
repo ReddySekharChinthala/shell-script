f19692cc47047698c2c9d12daae67be8be415ef1
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2026-02-03 23:20:23 +0530

    Hello-world

--- 06-array.sh
+++ 06-array.sh
@@ -2,7 +2,7 @@
 
 TOOLS=("SHELL-Script", "Ansible", "Terraform")
 
-echo "Scripting tool is: ${TOOLS [0]}"
-echo "Configuration tool is: ${TOOLS [1]}"
-echo "IAC tool is: ${TOOLS [2]}"
-echo "All tools are: ${TOOLS [@]}"
+echo "Scripting tool is: ${TOOLS[0]}"
+echo "Configuration tool is: ${TOOLS[1]}"
+echo "IAC tool is: ${TOOLS[2]}"
+echo "All tools are: ${TOOLS[@]}"
