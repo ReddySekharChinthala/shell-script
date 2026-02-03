2096af6285384127e75569f0ae8ac9c46e470b4e
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2026-02-03 22:52:46 +0530

    Hello-world

---
+++ 03-variables-reffering.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring variables
+Person1=Reddy
+Person1=Sekhar
+
+#reffering variables
+echo "$Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hi $Person1, Im good. What about you?"
+echo "$Person1:: Im also good and i heard you are learining devops course"
+echo "$Person2:: Yes, actually i want to get a devops job this year"
