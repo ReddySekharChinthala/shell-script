4baef179238e2c85d1e0dcc784bb5d1f3ede6c3a
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2026-02-03 23:46:56 +0530

    Hello-world

---
+++ 09-special-veriables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "All the varibles: $@"
+echo "Number of variales passed: $#"
+echo "Current script name: $0"
+echo "Current working directory: $PWD"
+echo "Current home directory: $HOME"
+echo "Current directory username: $USER"
+echo "hostname: $HOSTNAME"
+echo "Process ID of current shell-script: $$"
