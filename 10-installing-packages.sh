2dd7d2067d001ff258606e1b915e38a89e96e1f3
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2026-02-03 23:58:26 +0530

    Hello-world

--- 10-installing-packages.sh
+++ 10-installing-packages.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "Please run the script with root access."
+else    
+    echo "You are a super user."
+fi
+
 dnf install mysql -y
