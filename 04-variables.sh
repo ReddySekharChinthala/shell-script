7748f52c4ff639bba2de3550741e4b56ff3e7988
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2026-02-03 22:57:08 +0530

    Hello-world

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hi $Person1, Im good. What about you?"
+echo "$Person1:: Im also good and i heard you are learining devops course"
+echo "$Person2:: Yes, actually i want to get a devops job this year"
