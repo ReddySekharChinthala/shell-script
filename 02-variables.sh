1c1b5a82d44d6493affa8459de2582f1e704a383
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2026-02-03 22:42:13 +0530

    Hello-world

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Reddy:: Hi Sekhar, How are you?"
+echo "Sekhar:: Hi Reddy, Im good. What about you?"
+echo "Reddy:: Im also good and i heard you are learining devops course"
+echo "Sekhar:: Yes, actually i want to get a devops job this year"
