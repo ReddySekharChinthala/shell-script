f74df708915b603787eaa445e30cedf7b23b1665
ReddySekharChinthala <reddysekharchinthala@gmail.com>
2026-02-08 12:34:06 +0530

    functions

--- 12.logs.sh
+++ 12.logs.sh
@@ -23,8 +23,8 @@ else
     echo "Your super user"
 fi
 
-dnf install mysql -y &>>LOGFILE
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MYSQL"
 
-dnf install git -y &>>LOGFILE
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing Git"
